10049ff374e0a8c9dcf0b665f9a5861b9f6fe076
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-07-08 15:02:51 +0200

    - zad6

--- src/main/java/com/sda/advanced/solution/zad6/Main.java
+++ src/main/java/com/sda/advanced/solution/zad6/Main.java
@@ -1,5 +1,19 @@
 package com.sda.advanced.solution.zad6;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
+
+	//or <Object, Object>
+	public static void printFirstAndLast(TreeMap<?, ?> map) {
+		System.out.println(map.firstEntry());
+		System.out.println(map.lastEntry());
+	}
+
+	public static void main(String[] args) {
+		printFirstAndLast(new TreeMap<>(Map.of("a", 10, "z", 2, "c", 3)));
+		printFirstAndLast(new TreeMap<>(Map.of(5, 10, 4, 2, 1, 3)));
+	}
 
 }
