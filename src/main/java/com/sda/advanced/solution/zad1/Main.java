336580bbcfb30ebeb137228b8a2eda39b6069aa2
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-07-08 10:35:22 +0200

    -- zad1 done

--- src/main/java/com/sda/advanced/solution/zad1/Main.java
+++ src/main/java/com/sda/advanced/solution/zad1/Main.java
@@ -21,10 +21,45 @@ public class Main {
 		return sorted;
 	}
 
+	private static List<String> sortWithList(List<String> unsorted) {
+		List<String> sorted = new ArrayList<>(unsorted);
+		//Collections.reverse(sorted); - not good, just reverses the order of elements
+		sorted.sort(Comparator.reverseOrder());
+		return sorted;
+	}
+	private static List<String> sortWithMyComparator(List<String> unsorted) {
+		List<String> sorted = new ArrayList<>(unsorted);
+		sorted.sort(new MyStringComparator());
+		return sorted;
+	}
+
+	private static List<String> sortWithAnonymousClass(List<String> unsorted) {
+		List<String> sorted = new ArrayList<>(unsorted);
+		sorted.sort(new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				return -o1.compareTo(o2);
+			}
+		});
+		return sorted;
+	}
+
+	private static List<String> sortWithLambda(List<String> unsorted) {
+		List<String> sorted = new ArrayList<>(unsorted);
+		sorted.sort((o1, o2) -> -o1.compareTo(o2));
+		return sorted;
+	}
+
 	public static void main(String[] args) {
 		List<String> unsorted = List.of("A", "Z", "B", "d", "a", "c");
 		System.out.println(unsorted);
 		System.out.println(sortWithCollections(unsorted));
+		System.out.println(sortWithList(unsorted));
+		System.out.println(sortWithMyComparator(unsorted));
+		System.out.println(sortWithAnonymousClass(unsorted));
+		System.out.println(sortWithLambda(unsorted));
+
 		System.out.println(sortWithStream(unsorted));
 	}
 
