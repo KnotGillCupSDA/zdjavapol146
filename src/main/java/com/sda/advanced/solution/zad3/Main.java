7c6b77f7871fdeb572cdf9c4bd9757513b2e0ca6
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-07-08 11:51:01 +0200

    -- zad3 done

--- src/main/java/com/sda/advanced/solution/zad3/Main.java
+++ src/main/java/com/sda/advanced/solution/zad3/Main.java
@@ -1,11 +1,31 @@
 package com.sda.advanced.solution.zad3;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
 
 	private static void print(Map<String, Integer> map) {
+		/*
+		//Can be also done like this
+		for (String key : map.keySet()) {
+			Integer value = map.get(key);
+		}*/
+
+		int index = 0;
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			String key = entry.getKey();
+			Integer value = entry.getValue();
+			boolean isLastElement = index == map.size() -1;
+			if(isLastElement) {
+				System.out.println("Klucz: " + key + ", Wartość: " + value + ".");
+			} else {
+				System.out.println("Klucz: " + key + ", Wartość: " + value + ",");
+				//System.out.printf("Klucz: %s, Wartość: %d,\n", key, value);
+			}
+			index++;
+		}
 
 	}
 
