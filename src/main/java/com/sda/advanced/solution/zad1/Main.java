bc7b1bb8f9ccd851dc991ff540d7db10aa76edbe
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-07-08 10:08:53 +0200

    -- zad1 midwork2

--- src/main/java/com/sda/advanced/solution/zad1/Main.java
+++ src/main/java/com/sda/advanced/solution/zad1/Main.java
@@ -2,14 +2,22 @@ package com.sda.advanced.solution.zad1;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	public static List<String> sort(List<String> unsorted) {
 		//sort the list
 
-		return sortWithCollections(unsorted);
+		return sortWithStream(unsorted);
+	}
+
+	private static List<String> sortWithStream(List<String> unsorted) {
+		return unsorted.stream()
+				.sorted(Comparator.reverseOrder())
+				.collect(Collectors.toList());
 	}
 
 	private static List<String> sortWithCollections(List<String> unsorted) {
