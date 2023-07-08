dac8607daab63917b8c027d25e7b7904039443b1
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-07-08 11:13:41 +0200

    -- zad2 done

--- src/main/java/com/sda/advanced/solution/zad2/Main.java
+++ src/main/java/com/sda/advanced/solution/zad2/Main.java
@@ -1,18 +1,28 @@
 package com.sda.advanced.solution.zad2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
 	private static List<String> sortCaseInsensitive(List<String> unsorted) {
+		ArrayList<String> sorted = new ArrayList<>(unsorted);
+		Collections.sort(sorted, String.CASE_INSENSITIVE_ORDER.reversed());
+		return sorted;
+	}
 
-		return unsorted;
+	private static List<String> sortCaseInsensitiveWithLambda(List<String> unsorted) {
+		List<String> sorted = new ArrayList<>(unsorted);
+		sorted.sort((o1, o2) -> -o1.compareToIgnoreCase(o2));
+		return sorted;
 	}
 
 	public static void main(String[] args) {
 		List<String> unsorted = List.of("A", "Z", "B", "d", "a", "c");
 		System.out.println(unsorted);
 		System.out.println(sortCaseInsensitive(unsorted));
+		System.out.println(sortCaseInsensitiveWithLambda(unsorted));
 	}
 
 }
