944239c13c716be107a5d28a7d258d9ad70fd292
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-07-09 14:04:43 +0200

    - zad17 draft

---
+++ src/main/java/com/sda/advanced/solution/zad17/ConversionType.java
@@ -0,0 +1,10 @@
+package com.sda.advanced.solution.zad17;
+
+public enum ConversionType {
+	METERS_TO_YARDS,
+	YARDS_TO_METERS,
+	CENTIMETERS_TO_INCHES,
+	INCHES_TO_CENTIMETERS,
+	KILOMETERS_TO_MILES,
+	MILES_TO_KILOMETERS
+}
---
+++ src/main/java/com/sda/advanced/solution/zad17/MeasurementConverter.java
@@ -0,0 +1,8 @@
+package com.sda.advanced.solution.zad17;
+
+public class MeasurementConverter {
+
+	public double convert(double value, ConversionType conversionType) {
+		return 0.0;
+	}
+}
