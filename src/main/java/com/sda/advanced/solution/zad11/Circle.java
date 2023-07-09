523d3a93db19a9c247592a429242b57eb005f3f3
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-07-09 12:21:37 +0200

    - zad11 readme

---
+++ src/main/java/com/sda/advanced/solution/zad11/Circle.java
@@ -0,0 +1,32 @@
+package com.sda.advanced.solution.zad11;
+
+class Circle implements Movable {
+
+	private final Point2D center;
+	private final Point2D point;
+
+	Circle(Point2D center, Point2D point) {
+		this.center = center;
+		this.point = point;
+	}
+
+	public double getRadius() {
+		double dx = point.getX() - center.getX();
+		double dy = point.getY() - center.getY();
+		return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+	}
+
+	public double getPerimeter() {
+		return 2 * Math.PI * getRadius();
+	}
+
+	public double getArea() {
+		return Math.PI * Math.pow(getRadius(), 2);
+	}
+
+	@Override
+	public void move(MoveDirection moveDirection) {
+		center.move(moveDirection);
+		point.move(moveDirection);
+	}
+}
---
+++ src/main/java/com/sda/advanced/solution/zad11/MoveDirection.java
@@ -0,0 +1,29 @@
+package com.sda.advanced.solution.zad11;
+
+class MoveDirection {
+
+	private double x;
+	private double y;
+
+	public MoveDirection(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+}
