3c9bbc803289bbe3eef615e94f2622af1aebc168
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-07-15 14:25:21 +0200

    - zad31 done

--- src/main/java/com/sda/advanced/solution/zad31/Main.java
+++ src/main/java/com/sda/advanced/solution/zad31/Main.java
@@ -5,18 +5,41 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		Path workingDir = Paths.get("src", "main", "resources", "zad31");
-		Path originalFile = workingDir.resolve("zadania.txt");
-		byMichal(originalFile.toString(),
-				originalFile.getParent().resolve("out.txt").toString());
+		Path inputFile = workingDir.resolve("zadania.txt");
+		Path outputFile = inputFile.getParent().resolve("out.txt");
+		//byMichal(inputFile.toString(), outputFile.toString());
+		withNio(inputFile, outputFile);
+	}
+
+	private static void withNio(Path inputFile, Path outputFile) throws IOException {
+		Map<String, Integer> wordCountMap = getWordCountMap(inputFile);
+		List<String> outputLines = wordCountMap.entrySet().stream()
+				.map(e -> "|" + e.getKey() + "|" + e.getValue() + '|')
+				.collect(Collectors.toList());
+		Files.write(outputFile, outputLines);
+	}
+
+	private static Map<String, Integer> getWordCountMap(Path inputFile) throws IOException {
+		Map<String, Integer> wordCountMap = new HashMap<>();
+		for (String line : Files.readAllLines(inputFile)) {
+			String[] words = line.split("\\s+");
+			for (String word : words) {
+				wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+			}
+		}
+		return wordCountMap;
 	}
 
 	private static void byMichal(String inputFile, String outputFile) throws IOException {
