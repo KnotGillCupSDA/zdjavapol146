9a3a8221000b2ba6ea3eaaf50b5124c2c252b0b3
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2023-07-15 11:36:42 +0200

    - I/O playground

---
+++ src/main/java/com/sda/advanced/solution/io/Playground.java
@@ -0,0 +1,32 @@
+package com.sda.advanced.solution.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class Playground {
+
+	public static void main(String[] args) throws IOException {
+		String absolutPath = "C:\\priv\\dev\\SDA\\zdjavapol146\\src\\main\\resources\\zad30\\someTextFile.txt";
+		String workingDir = "src/main/resources/zad30";
+		String relativePath = workingDir + "someTextFile.txt";
+		String destinationPath = workingDir + "destination.txt";
+
+		Path resourceDir = Paths.get("src", "main", "resources");
+		Path zad30dir = resourceDir.resolve("zad30");
+
+		Path relativePath2 = zad30dir.resolve("someTextFile.txt");
+
+		System.out.println(Files.readString(relativePath2));
+
+		List<String> lines = Files.readAllLines(relativePath2);
+		System.out.println(lines);
+
+		Files.writeString(zad30dir.resolve("newFile.txt"), "cokolwiek3", StandardOpenOption.APPEND);
+
+	}
+
+}
